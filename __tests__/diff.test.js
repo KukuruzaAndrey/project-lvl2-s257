7ee12d4fce011186ba72baa40fd77c66f7e1b9db
Andrey Kukuruza <andreyku6@gmail.com>
2018-05-18 11:04:30 +0300

    add nested join

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -3,14 +3,16 @@ import path from 'path';
 import genDiff from '../src';
 
 const diffTest = (before, after, expectedResult) => {
-  const pathToFile = `${__dirname}/__fixtures__/simple/`;
+  const pathToFile = `${__dirname}/__fixtures__`;
   const diff = genDiff(path.resolve(pathToFile, before), path.resolve(pathToFile, after));
   const result = fs.readFileSync(path.resolve(pathToFile, expectedResult), 'utf-8');
   expect(diff).toBe(result);
 };
 
-test('simple json', () => diffTest('before.json', 'after.json', 'expectedResult'));
+test('simple json', () => diffTest('simple/before.json', 'simple/after.json', 'simple/expectedResult'));
 
-test('simple yaml', () => diffTest('before.yaml', 'after.yaml', 'expectedResult'));
+test('simple yaml', () => diffTest('simple/before.yaml', 'simple/after.yaml', 'simple/expectedResult'));
 
-test('simple ini', () => diffTest('before.ini', 'after.ini', 'expectedResult'));
+test('simple ini', () => diffTest('simple/before.ini', 'simple/after.ini', 'simple/expectedResult'));
+
+test('nested json', () => diffTest('nested/before.json', 'nested/after.json', 'nested/expectedResult'));
--- src/index.js
+++ src/index.js
@@ -8,20 +8,82 @@ const genDiff = (path1, path2) => {
   const parse = getParser(ext);
   const obj1 = parse(readFileSync(path1, 'utf-8'));
   const obj2 = parse(readFileSync(path2, 'utf-8'));
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-  const body = keys.map((key) => {
-    if (!_.has(obj1, key)) {
-      return `+ ${key}: ${obj2[key]}`;
-    } else if (!_.has(obj2, key)) {
-      return `- ${key}: ${obj1[key]}`;
-    } else if (obj1[key] === obj2[key]) {
-      return `  ${key}: ${obj1[key]}`;
-    } else if (obj1[key] !== obj2[key]) {
-      return [`- ${key}: ${obj1[key]}`, `+ ${key}: ${obj2[key]}`];
-    }
-    return [];
-  });
-  return `{\n    ${_.flatten(body).join('\n    ')}\n}`;
+
+  const diffAST = (before, after) => {
+    const keys = _.union(_.keys(before), _.keys(after));
+    return _.flatten(keys.map((key) => {
+      if (_.isObject(before[key]) && _.isObject(after[key])) {
+        return {
+          name: key,
+          value: '',
+          status: 'unchanged',
+          children: diffAST(before[key], after[key]),
+        };
+      } else if (!_.has(before, key)) {
+        return {
+          name: key,
+          value: after[key],
+          status: 'added',
+          children: [],
+        };
+      } else if (!_.has(after, key)) {
+        return {
+          name: key,
+          value: before[key],
+          status: 'removed',
+          children: [],
+        };
+      } else if (before[key] === after[key]) {
+        return {
+          name: key,
+          value: after[key],
+          status: 'unchanged',
+          children: [],
+        };
+      } else if (before[key] !== after[key]) {
+        return [{
+          name: key,
+          value: before[key],
+          status: 'removed',
+          children: [],
+        }, {
+          name: key,
+          value: after[key],
+          status: 'added',
+          children: [],
+        }];
+      }
+      return [];
+    }));
+  };
+
+
+  const render = (ast) => {
+    const stringify = (obj, depth) => {
+      if (!_.isObject(obj)) {
+        return obj;
+      }
+      return `{\n${Object.keys(obj).map(key => `${'    '.repeat(depth + 2)}${key}: ${obj[key]}`).join('\n')}\n${'    '.repeat(depth + 1)}}`;
+    };
+    const statusMap = {
+      added: '+',
+      removed: '-',
+      unchanged: ' ',
+    };
+    const iter = (acc, node, depth) => {
+      const {
+        name, value, status, children,
+      } = node;
+      if (_.isEmpty(children)) {
+        return [...acc, `  ${'    '.repeat(depth)}${statusMap[status]} ${name}: `, `${stringify(value, depth)}\n`];
+      }
+      return [...acc, `  ${'    '.repeat(depth)}${statusMap[status]} ${name}: {\n`, ...children.reduce((nAcc, n) => iter(nAcc, n, depth + 1), []), `${'    '.repeat(depth + 1)}}\n`];
+    };
+    const body = ast.reduce((acc, node) => iter(acc, node, 0), []);
+    return `{\n${body.join('')}}`;
+  };
+  console.log(JSON.stringify(diffAST(obj1, obj2)));
+  return render(diffAST(obj1, obj2));
 };
 
 export default genDiff;
