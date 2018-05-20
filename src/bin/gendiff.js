261e8340bbf983119d698d2b1aa0b4db194b9aae
Andrey Kukuruza <andreyku6@gmail.com>
2018-05-20 18:29:54 +0300

    change version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@ import program from 'commander';
 import genDiff from '..';
 
 program
-  .version('1.0.0')
+  .version('2.0.0')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
