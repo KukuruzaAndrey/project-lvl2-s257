d1afe3363f8960bb9e84999f0f0058a992e6b407
Andrey Kukuruza <andreyku6@gmail.com>
2018-05-20 16:55:18 +0300

    refactor standartRender

--- src/renderers/standardRender.js
+++ src/renderers/standardRender.js
@@ -9,16 +9,15 @@ const StandardRender = (ast) => {
     return `{\n${body}${'    '.repeat(depth + 1)}}`;
   };
   const render = {
-    nested: ({ name, children }, depth, func) => [`${'    '.repeat(depth)}  ${name}: {\n`, ...children.reduce((nAcc, n) => func(nAcc, n, depth + 1), []), `${'    '.repeat(depth + 1)}}`].join(''),
-    added: ({ name, value }, depth) => `${'    '.repeat(depth)}+ ${name}: ${stringify(value, depth)}`,
-    removed: ({ name, value }, depth) => `${'    '.repeat(depth)}- ${name}: ${stringify(value, depth)}`,
-    unchanged: ({ name, value }, depth) => `${'    '.repeat(depth)}  ${name}: ${stringify(value, depth)}`,
-    updated: ({ name, value: { before, after } }, depth) => [`${'    '.repeat(depth)}- ${name}: ${stringify(before, depth)}`,
-      `${'    '.repeat(depth)}  + ${name}: ${stringify(after, depth)}`].join('\n'),
+    nested: ({ name, children }, depth, func) => [`  ${name}: {\n`, ...children.reduce((nAcc, n) => func(nAcc, n, depth + 1), []), `${'    '.repeat(depth + 1)}}`].join(''),
+    added: ({ name, value }, depth) => `+ ${name}: ${stringify(value, depth)}`,
+    removed: ({ name, value }, depth) => `- ${name}: ${stringify(value, depth)}`,
+    unchanged: ({ name, value }, depth) => `  ${name}: ${stringify(value, depth)}`,
+    updated: ({ name, value: { before, after } }, depth) => [`- ${name}: ${stringify(before, depth)}`, `${'    '.repeat(depth)}  + ${name}: ${stringify(after, depth)}`].join('\n'),
   };
   const iter = (acc, node, depth) => {
     const { type } = node;
-    return [...acc, `  ${render[type](node, depth, iter)}\n`];
+    return [...acc, `  ${'    '.repeat(depth)}${render[type](node, depth, iter)}\n`];
   };
   const body = ast.reduce((acc, node) => iter(acc, node, 0), []);
   return `{\n${body.join('')}}`;
