628aa9136d0e75aca786dc82d1344f8ef7d4c773
Andrey Kukuruza <andreyku6@gmail.com>
2018-05-21 12:23:49 +0300

    mini refactor build AST function

--- src/index.js
+++ src/index.js
@@ -4,38 +4,38 @@ import _ from 'lodash';
 import getParser from './parser';
 import getRender from './renderers';
 
+const keyTypes = [
+  {
+    type: 'nested',
+    check: (before, after, key) => _.isObject(before[key]) && _.isObject(after[key]),
+    action: (before, after, func) => ({ children: func(before, after) }),
+  },
+  {
+    type: 'added',
+    check: (before, after, key) => !_.has(before, key),
+    action: (before, after) => ({ value: after }),
+  },
+  {
+    type: 'removed',
+    check: (before, after, key) => !_.has(after, key),
+    action: before => ({ value: before }),
+  },
+  {
+    type: 'unchanged',
+    check: (before, after, key) => before[key] === after[key],
+    action: (before, after) => ({ value: after }),
+  },
+  {
+    type: 'updated',
+    check: (before, after, key) => before[key] !== after[key],
+    action: (before, after) => ({ valueBefore: before, valueAfter: after }),
+  },
+];
 const genDiff = (path1, path2, renderType = 'standard') => {
   const ext = path.extname(path1);
   const parse = getParser(ext);
   const obj1 = parse(readFileSync(path1, 'utf-8'));
   const obj2 = parse(readFileSync(path2, 'utf-8'));
-  const keyTypes = [
-    {
-      type: 'nested',
-      check: (before, after, key) => _.isObject(before[key]) && _.isObject(after[key]),
-      action: (before, after, func) => ({ children: func(before, after) }),
-    },
-    {
-      type: 'added',
-      check: (before, after, key) => !_.has(before, key),
-      action: (before, after) => ({ value: after }),
-    },
-    {
-      type: 'removed',
-      check: (before, after, key) => !_.has(after, key),
-      action: before => ({ value: before }),
-    },
-    {
-      type: 'unchanged',
-      check: (before, after, key) => before[key] === after[key],
-      action: (before, after) => ({ value: after }),
-    },
-    {
-      type: 'updated',
-      check: (before, after, key) => before[key] !== after[key],
-      action: (before, after) => ({ valueBefore: before, valueAfter: after }),
-    },
-  ];
   const buildAST = (before, after) => {
     const keys = _.union(_.keys(before), _.keys(after));
     return keys.map((key) => {
